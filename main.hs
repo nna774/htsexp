c502ea0e31e5782368130ca3df928b8c65203d33
NoNameA 774 <nonamea.774@gmail.com>
2014-01-02 01:11:11 +0900

    Hlint に指摘されたとこなおしました

--- main.hs
+++ main.hs
@@ -2,6 +2,7 @@ import Text.Parsec
 import Text.Parsec.String
 import Text.Parsec.Prim as Prim
 import Data.List
+import Control.Monad
 
 data HtSExp = Elem String [HtSExp] | Attr String String | Str String | Comment String deriving (Show, Read, Eq)
 
@@ -43,11 +44,11 @@ quote :: Parser String
 quote = char '"' >> many (escapedChars <|> noneOf "\"\\") >>= (\x -> char '"' >> return x)
 
 str :: Parser HtSExp
-str = quote >>= (return . Str)
+str = liftM Str quote
 
 -- | (* Fill comments here *)
 comment :: Parser HtSExp
-comment = string "(*" >> manyTill anyChar (Prim.try (string "*)")) >>= (return . Comment)
+comment = liftM Comment (string "(*" >> manyTill anyChar (Prim.try (string "*)")))
 
 readHtSExp :: String -> String
 readHtSExp input = case parse (element >>= (\x -> spaces >> eof >> return x) ) "HtSExp" input of
@@ -58,13 +59,16 @@ convertToHtml :: HtSExp -> String
 convertToHtml (Elem e []) = "<" ++ e ++ " />"
 convertToHtml (Elem "html" xs) = "<!doctype html>" ++ convertToHtml' (Elem "html" xs) 
 convertToHtml e@(Elem _ _) = convertToHtml' e
-convertToHtml (Str s) = s --"\"" ++ s ++ "\""
+convertToHtml (Str s) = s
 convertToHtml (Comment c) = "<!-- " ++ c ++ "-->"
 
 convertToHtml' :: HtSExp -> String
-convertToHtml' (Elem e xs) = case others == [] && e `notElem` elemlist of
-                               True  -> "<" ++ e ++ concat (map flattenAttr attrs) ++ " />"
-                               False -> "<" ++ e ++ concat (map flattenAttr attrs) ++ ">" ++ concat (map convertToHtml others) ++ "</" ++ e ++ ">"
+convertToHtml' (Elem e xs) = "<" ++ e ++ concatMap flattenAttr attrs ++ 
+    if null others && e `notElem` elemlist
+    then
+        " />"
+    else
+        ">" ++ concatMap convertToHtml others ++ "</" ++ e ++ ">"
     where (attrs, others) = partition isAttr xs
           flattenAttr (Attr att val) = " " ++ att ++ "=\"" ++ val ++ "\""
 
@@ -72,7 +76,7 @@ isAttr :: HtSExp -> Bool
 isAttr (Attr _ _) = True
 isAttr _ = False
 
-elemlist :: [String]
+elemlist :: [String] -- "/>" で閉じると怒られるタグ
 elemlist = ["script"]
 
 main :: IO ()
